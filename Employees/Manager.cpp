4b0822ba58a892470e634ae626d5292e95fea2de
Giulia Mescolini <65012808+giuliamesc@users.noreply.github.com>
2020-10-28 10:59:11 +0100

    Update Manager.cpp

--- Employees/Manager.cpp
+++ Employees/Manager.cpp
@@ -2,5 +2,10 @@
 
 double Manager::salary_cal (void) const
 {
-    // TODO: implement this method
+    return (work_hours - wsh_hours - m_hours)*pay_rate+wsh_rate*wsh_hours+m_hours*m_rate;
+}
+
+void Manager::set_m_hours(unsigned int n)
+{
+    m_hours=n;
 }
