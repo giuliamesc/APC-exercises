1e85463fe1a848cad5117d3829e4f6a6aab67156
Giulia Mescolini <65012808+giuliamesc@users.noreply.github.com>
2020-12-04 22:33:38 +0100

    Add files via upload

---
+++ Exams/FileManager.cpp
@@ -0,0 +1,23 @@
+//
+// Created by giuli on 04/12/2020.
+//
+#include "FileManager.h"
+
+typedef std::vector<std::string> row_type;
+typedef std::vector<row_type> table_type;
+
+const table_type& FileManager::parse_file (const std::string& filename, char d){
+    std::ifstream data {filename};
+    std::string line;
+    while(std::getline(data,line)){
+        row_type temp;
+        std::istringstream record(line);
+        std::string word;
+        while (getline(record, word, d)){
+            temp.push_back(word);
+        }
+        fields.emplace_back(temp);
+    }
+    const table_type& ret =fields;
+    return ret;
+}
---
+++ Exams/main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <unordered_map>
+
+#include "FileManager.h"
+
+using std::unordered_map;
+using std::vector;
+using std::string;
+using std::cout;
+using std::endl;
+using std::cerr;
+
+typedef vector<string> row_type;
+typedef vector<row_type> table_type;
+typedef unordered_map<unsigned,StudentsData> students_type;
+
+void add_students (const table_type&, students_type&);
+void add_exams (const table_type&, students_type&);
+
+int main () {
+
+    string filename_students = "../students.txt";
+    string filename_exams = "../exams.txt";
+
+    students_type students;
+
+    FileManager FM;
+
+    // parse students file
+    table_type fields = FM.parse_file(filename_students);
+
+    // add students to the map
+    add_students(fields, students);
+
+    // parse exams file
+    fields = FM.parse_file(filename_exams);
+
+    // add exams to the corresponding students, if they exist in the map
+    add_exams(fields, students);
+
+    // compute and print average of students' grades
+    float sum=0;
+
+    for(const auto & student : students){
+        sum+=student.second.average_grade();
+    }
+
+    float avg=sum/students.size();
+
+    std::cout << "Average grade of all the students: " << avg << std::endl;
+
+    for(const auto & student : students){
+        std::cout << "Student id: " << student.first << " Average: " << student.second.average_grade() << std::endl;
+    }
+
+}
+
+void add_students (const table_type& fields, students_type& students) {
+    for(auto my_row : fields){
+    unsigned id=std::stoul(my_row[0]);
+    StudentsData new_stud(my_row[1],my_row[2],my_row[3]);
+    students.insert(std::make_pair(id,new_stud));
+    }
+}
+
+void add_exams (const table_type& fields, students_type& students) {
+    for(auto my_row : fields){
+        unsigned id=std::stoul(my_row[0]);
+        auto ptr=students.find(id);
+        row_type row1=fields[1];
+        //Exam my_exam(std::stoul(my_row[1]),my_row[2],std::stoul(my_row[3])); //Here is the mistake
+        if(ptr!=students.end()){
+            //ptr->second.add_exam(my_exam);
+        }
+        else
+            std::cout << "Student not found" << std::endl;
+    }
+}
+
