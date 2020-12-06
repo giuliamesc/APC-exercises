40d1fb1e1b39626a7daa37dd86c9d1b09b30c907
Giulia Mescolini <65012808+giuliamesc@users.noreply.github.com>
2020-12-06 14:42:04 +0100

    Update main.cpp

--- Exams/main.cpp
+++ Exams/main.cpp
@@ -64,16 +64,18 @@ void add_students (const table_type& fields, students_type& students) {
 }
 
 void add_exams (const table_type& fields, students_type& students) {
-    for(auto my_row : fields){
+    unsigned n_start=students.size();
+    for(unsigned j=n_start; j<fields.size(); j++){
+        row_type my_row=fields[j];
         unsigned id=std::stoul(my_row[0]);
         auto ptr=students.find(id);
-        row_type row1=fields[1];
-        //Exam my_exam(std::stoul(my_row[1]),my_row[2],std::stoul(my_row[3])); //Here is the mistake
+        Exam my_exam(std::stoul(my_row[1]),my_row[2],std::stoul(my_row[3]));
         if(ptr!=students.end()){
-            //ptr->second.add_exam(my_exam);
+            ptr->second.add_exam(my_exam);
         }
         else
             std::cout << "Student not found" << std::endl;
     }
 }
+
 
